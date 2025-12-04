87cc6e06daa7d00d3af0d2a5345767883bb46f80
Ethan Omessi <ethanomessi@gmail.com>
2025-12-04 15:41:15 -0600

    EOguest done

--- scripts/guests.js
+++ scripts/guests.js
@@ -1,3 +1,16 @@
 // import getGuests from database
 // build an html list of guests
 // export guestList function to return html
+import { getGuest } from "./database"
+
+export const guestList = () => {
+    const guest = getGuest()
+    let guestsHTML = `<div id="guestbody"><h2>Guests</h2>`
+
+    for (const guest of guests) {
+        guestsHTML += `<p data-id="${guest.id}">${guest.name}</p>`
+    }
+    
+    guestsHTML += "</div>"
+    return guestsHTML
+}
