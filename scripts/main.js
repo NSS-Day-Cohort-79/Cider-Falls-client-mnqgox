e3c70725265da6160a95c4df3b652ad45604cfd9
Jordan Haddock <haddockjordan40@gmail.com>
2025-12-05 15:36:03 -0600

    Did some work to the css files and added the park areas module

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,29 @@
-//import { parkAreaList } from './parkAreas.js';
+import { parkAreaList } from './parkAreas.js';
 import { guestList } from './guests.js';
-//import { serviceList } from './services.js';
+import { serviceList } from './services.js';
 
-const parkAreasListHTML = "" // = parkAreaList();
+const parkAreasListHTML = parkAreaList();
 const guestListHTML = guestList();
-//const servicesListHTML = serviceList();
+const servicesListHTML = serviceList();
 
 const mainContainer = document.querySelector("#container");
 
 mainContainer.innerHTML = `
     <body>
-    <header>
-        <h1>Cider Falls State Park</h1>
         <section class="services">
             Park services: ${servicesListHTML}
         </section>
         <section class="areas">
-        <h2>Park Areas</h2>
-       ParkAreas: {parkAreasListHTML}
+            <h2>Park Areas</h2>
+            ${parkAreasListHTML}
         </section>
-        </header>
-   
-        `;
-        guestContainer.innerHTML = document.querySelector("#guestsContainer").innerHTML;
-        guestContainer.innerHTML = guestListHTML;`
-        <body>
-        <header>
+            <body>
+            <header>
         <aside class="guests">
-        Guests: ${guestListHTML}
-        </aside>
+            ${guestListHTML}
+            </aside>
         <footer>
-
-        <p>Main Phone: (XXX)XXX-XXXX, Email Address: info@ciderfallspark.com Address: 123 Cider Falls Rd, Cider Falls, CF 12345</p>
-    </footer>
+            <p>Main Phone: (XXX)XXX-XXXX, Email Address: info@ciderfallspark.com Address: 123 Cider Falls Rd, Cider Falls, CF 12345</p>
+        </footer>
     </body>
     `;
--- scripts/parkAreas.js
+++ scripts/parkAreas.js
@@ -1,9 +1,29 @@
-import {getArea} from '../scripts/database.js';
-import { getServices } from "./database";
+import {getArea, getGuest} from '../scripts/database.js';
 import { serviceList } from './services.js';
 
+const parkAreas = getArea();
+const allGuests = getGuest();
 
-return parkAreas = getArea();
-const allServices = getServices();
+document.addEventListener("click", clickEvent => {
+    if (clickEvent.target.className === "area-title") {
+        const clickedAreaID = parseInt(clickEvent.target.dataset.id)
+        let guestCount = 0
+        for (const guest of allGuests) {
+            if (guest.areaID === clickedAreaID) {
+                guestCount++
+            }
+        }
+        window.alert(`There are ${guestCount} guests in this area`)
+    }
+})
 
-export const servicesList(areas)
+export const parkAreaList = () => {
+    let areaHTML = "";
+    for (const area of parkAreas) {
+        areaHTML += `<section class="park-area">
+            <h3 class="area-title" data-id="${area.id}">${area.title}</h3>
+            <div class="area-services">${serviceList(area)}</div>
+        </section>`
+    }
+    return areaHTML;
+}
