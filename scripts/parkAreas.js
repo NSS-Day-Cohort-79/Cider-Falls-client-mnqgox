acafc4dfd6b60bb4923c582dc22e44f4fa08a3ea
Jordan Haddock <haddockjordan40@gmail.com>
2025-12-05 14:57:06 -0600

    working on areas

---
+++ scripts/parkAreas.js
@@ -0,0 +1,9 @@
+import {getArea} from '../scripts/database.js';
+import { getServices } from "./database";
+import { serviceList } from './services.js';
+
+
+return parkAreas = getArea();
+const allServices = getServices();
+
+export const servicesList(areas)
